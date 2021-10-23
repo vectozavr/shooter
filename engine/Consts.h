6fba29513950908205bd45aeb66f76c3edac1c44
Vectozavr <60608292+vectozavr@users.noreply.github.com>
2021-10-23 21:15:24 +0700

    VS support

--- engine/Consts.h
+++ engine/Consts.h
@@ -14,6 +14,7 @@ namespace Consts {
     const std::string PROJECT_NAME = "engine";
     const bool USE_LOG_FILE = true;
     const bool USE_OPEN_GL = true;
+    const bool SHOW_COORDINATES = true;
 
     const double PI = 3.14159265358979323846264338327950288;
     const double EPS = 0.000001;
--- engine/Engine.h
+++ engine/Engine.h
@@ -18,8 +18,8 @@ private:
     std::string _name;
 
     double _triPerSec = 0;
-    bool _debugText = true;
     bool _updateWorld = true;
+    bool _debugText = Consts::SHOW_COORDINATES;
     bool _useOpenGL = Consts::USE_OPEN_GL;
 
     void printDebugText() const;
