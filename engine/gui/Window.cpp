6bf5e1c7907cc04e522b6b98f0b3e176aae05eea
Vectozavr <60608292+vectozavr@users.noreply.github.com>
2021-11-06 01:59:33 +0700

    pushGLStates & popGLStates optimizations

--- engine/gui/Window.cpp
+++ engine/gui/Window.cpp
@@ -18,6 +18,7 @@ void Window::addButton(int x, int y, int w, int h, std::function<void()> click, 
 
 void Window::update() {
 
+    _screen->pushGLStates();
     _screen->setTitle(_name);
     _screen->drawSprite(_back);
 
@@ -45,6 +46,7 @@ void Window::update() {
             _screen->drawText(button.text());
         }
     }
+    _screen->popGLStates();
 }
 
 void Window::setBackgroundTexture(const std::string &texture, double sx, double sy, int w, int h) {
